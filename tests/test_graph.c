2edb2ca6b58cd711a3d05f1baa6d6a809c087068
Traiko Dinev <traiko.dinev@gmail.com>
2026-05-27 22:30:28 +0100

    Add failing tests (accumulation).
    - Accumulation fails due to not carrying partials over but entire
      gradients. Add failing test case.

--- tests/test_graph.c
+++ tests/test_graph.c
@@ -62,9 +62,59 @@ static char *test_backward(void) {
     return 0;
 }
 
+static char *test_squared_loss() {
+    Node *x = init(1.5);
+    Node *a = init(2.0);
+    Node *b = init(0.5);
+    Node *y = init(10.0);
+
+    Node *diff = sub(y, add(mul(a, x), b));
+    Node *loss = mul(diff, diff);
+
+    forward(loss);
+    backward(loss, 1.0);
+
+    mu_assert_close("squared loss: da", a->grad, finite_diff(a, loss));
+    mu_assert_close("squared loss: db", b->grad, finite_diff(b, loss));
+    mu_assert_close("squared loss: dx", x->grad, finite_diff(x, loss));
+    mu_assert_close("squared loss: dy", y->grad, finite_diff(y, loss));
+    mu_assert_close("squared loss: ddiff", diff->grad, finite_diff(diff, loss));
+
+
+    free_node(loss);
+    return 0;    
+}
+
+
+static char *test_accumulation() {
+    Node *x = init(1.5);
+    Node *a = init(2.0);
+    Node *b = init(0.5);
+    Node *y = init(10.0);
+
+    Node *diff = sub(y, add(mul(a, x), b));
+    Node *loss = mul(diff, diff);
+
+    forward(loss);
+    backward(loss, 1.0);
+    backward(loss, 1.0);
+
+    mu_assert_close("accumulation: da", a->grad, 2 * finite_diff(a, loss));
+    mu_assert_close("accumulation: db", b->grad, 2 * finite_diff(b, loss));
+    mu_assert_close("accumulation: dx", x->grad, 2 * finite_diff(x, loss));
+    mu_assert_close("accumulation: dy", y->grad, 2 * finite_diff(y, loss));
+    mu_assert_close("accumulation: ddiff", diff->grad, 2 * finite_diff(diff, loss));
+
+    free_node(loss);
+    return 0;    
+}
+
+
 static char *all_tests(void) {
     mu_run_test(test_forward);
     mu_run_test(test_backward);
+    mu_run_test(test_squared_loss);
+    mu_run_test(test_accumulation);
     return 0;
 }
 
